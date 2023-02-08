13c7fd88cd0900f91a2d141e7e882d0b78baf9dd
awesome <pienariu.laurentiu@gmail.com>
2023-02-08 22:29:23 +0200

    Initial commit

---
+++ Multiples of 3 or 5/multiplesOf3or5.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+
+
+first = 1
+second = 2
+
+sum = 0
+
+while sum<4000000:
+    sum = first + second
+    first = second
+    second = sum
+    print(sum)
+
+print(type('s'))
