9a709cd6dcf53d32111c1725989afcffc1388ed6
awesome <pienariu.laurentiu@gmail.com>
2023-02-10 17:15:46 +0200

    final version

--- Multiples of 3 or 5/multiplesOf3or5.py
+++ Multiples of 3 or 5/multiplesOf3or5.py
@@ -1,12 +1,16 @@
 import math
 
+#Problem 1
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
 def multipleOf3or5(n):
     sum = 0
     for i in range(n):
         if (i%3 == 0) | (i%5 == 0):
             sum = sum + i
-    print(sum)
+    return sum
 
-print(multipleOf3or5(1000))
+print("The sum of multiples of 3 and 5 below 1000 is :", multipleOf3or5(1000))
 
 
